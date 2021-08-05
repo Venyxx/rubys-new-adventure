471b1e1eee72d4567bc51d47c23bf1db4eb5bc54
Venyxx <scottvanessa01@gmail.com>
2021-08-04 23:00:26 -0400

    August 4, 2021 11:00 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -146,7 +146,7 @@ Instantiate(backgroundm);
             for (int x = 0; x < cubeRandStorage; x++)
             {
                 //Debug.Log("running the placement loop");
-                Instantiate(cogPickup, gameObject.transform.position * Random.Range(0.2f, 1.0f), gameObject.transform.rotation);
+                Instantiate(cogPickup, gameObject.transform.position * Random.Range(0.2f, 0.4f), gameObject.transform.rotation);
             }
         }
         else if (cogCount > 5)
